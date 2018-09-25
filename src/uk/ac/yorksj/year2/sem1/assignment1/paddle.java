198c6b068880352300177b1242a177c753e24136
Alexander Bartram <alexander.bartram@yorksj.ac.uk>
2018-09-25 14:38:25 +0100

    Added paddle class

---
+++ src/uk/ac/yorksj/year2/sem1/assignment1/paddle.java
@@ -0,0 +1,27 @@
+package uk.ac.yorksj.year2.sem1.assignment1;
+
+public class paddle {
+	
+	private int width;
+	private int height;
+	
+	
+	public paddle() {
+		
+	}
+	
+	public paddle(int x, int y) {
+		this.width = x;
+		this.height = y;
+	}
+	
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	
+
+}
